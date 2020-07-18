aa0696e5a1baaedad60746c8520180391aa22489
kamome283 <26428614+kamome283@users.noreply.github.com>
2020-07-18 13:50:52 +0900

    Refactored fields naming convention.

--- AngleParse.Test/RegexSelectorTest.cs
+++ AngleParse.Test/RegexSelectorTest.cs
@@ -17,9 +17,9 @@ namespace AngleParse.Test
         private static readonly RegexSelector notMatchingSelector = new NotMatchingRegexSelector();
         private static readonly RegexSelector notCapturingSelector = new NotCapturingRegexSelector();
 
-        private static readonly ElementResource validElementResource = new ValidElementResource();
-        private static readonly ElementResource invalidElementResource = new InvalidElementResource();
-        private static readonly StringResource validStringResource = new ValidStringResource();
+        private static readonly ElementResource validResource = new ValidElementResource();
+        private static readonly ElementResource invalidResource = new InvalidElementResource();
+        private static readonly StringResource stringResource = new ValidStringResource();
 
         [Fact]
         public void InitializingInvalidSelectorThrowsTypeInitializationException()
@@ -33,21 +33,21 @@ namespace AngleParse.Test
         [Fact]
         public void SelectByNotCapturingSelectorReturnsEmptySequence()
         {
-            var actual = notCapturingSelector.Select(validStringResource).Select(r => r.AsString());
+            var actual = notCapturingSelector.Select(stringResource).Select(r => r.AsString());
             Assert.Empty(actual);
         }
 
         [Fact]
         public void SelectByNotMatchingSelectorReturnsEmptySequence()
         {
-            var actual = notMatchingSelector.Select(validStringResource).Select(r => r.AsString());
+            var actual = notMatchingSelector.Select(stringResource).Select(r => r.AsString());
             Assert.Empty(actual);
         }
 
         [Fact]
         public void SelectOnInvalidElementResourceWorks()
         {
-            var actual = validSelector.Select(invalidElementResource).Select(r => r.AsString());
+            var actual = validSelector.Select(invalidResource).Select(r => r.AsString());
             Assert.Empty(actual);
         }
 
@@ -59,7 +59,7 @@ namespace AngleParse.Test
                 "November", "2006", "Server", "2003", "Server", "2003", "Server", "2008", "Server", "2008", "August",
                 "2016", "January", "2018"
             };
-            var actual = validSelector.Select(validElementResource).Select(r => r.AsString());
+            var actual = validSelector.Select(validResource).Select(r => r.AsString());
             Assert.Equal(expected, actual);
         }
 
@@ -71,7 +71,7 @@ namespace AngleParse.Test
                 "November", "2006", "Server", "2003", "Server", "2008", "Server", "2016", "Server", "2008", "Server",
                 "2008", "Server", "2012", "Server", "2012",
             };
-            var actual = validSelector.Select(validStringResource).Select(r => r.AsString());
+            var actual = validSelector.Select(stringResource).Select(r => r.AsString());
             Assert.Equal(expected, actual);
         }
     }
--- AngleParse.Test/StringSelectorTest.cs
+++ AngleParse.Test/StringSelectorTest.cs
@@ -17,10 +17,10 @@ namespace AngleParse.Test
         private static readonly StringSelector notMatchingSelector = new NotMatchingStringSelector();
 
         // AngleSharp tries to parse even broken HTML, so that never throws exception.
-        private static readonly ElementResource invalidElementResource = new InvalidElementResource();
-        private static readonly ElementResource validElementResource = new ValidElementResource();
-        private static readonly ElementResource emptyElementResource = new EmptyElementResource();
-        private static readonly StringResource validStringResource = new ValidStringResource();
+        private static readonly ElementResource invalidResource = new InvalidElementResource();
+        private static readonly ElementResource validResource = new ValidElementResource();
+        private static readonly ElementResource emptyResource = new EmptyElementResource();
+        private static readonly StringResource stringResource = new ValidStringResource();
 
         [Fact]
         public void InitializingInvalidSelectorThrowsTypeInitializationException()
@@ -34,7 +34,7 @@ namespace AngleParse.Test
         [Fact]
         public void SelectByNotMatchingSelectorReturnsEmptySequence()
         {
-            var actual = notMatchingSelector.Select(validElementResource).Select(r => r.AsString());
+            var actual = notMatchingSelector.Select(validResource).Select(r => r.AsString());
             Assert.Empty(actual);
         }
 
@@ -42,28 +42,28 @@ namespace AngleParse.Test
         public void SelectByValidSelectorWorks()
         {
             var expected = new[] {"Windows XP SP2", "Windows Server 2003 SP1", "general availability"};
-            var actual = validSelector.Select(validElementResource).Select(r => r.AsString());
+            var actual = validSelector.Select(validResource).Select(r => r.AsString());
             Assert.Equal(expected, actual);
         }
 
         [Fact]
         public void SelectOnEmptyResourceReturnsEmptySequence()
         {
-            var actual = validSelector.Select(emptyElementResource).Select(r => r.AsString());
+            var actual = validSelector.Select(emptyResource).Select(r => r.AsString());
             Assert.Empty(actual);
         }
 
         [Fact]
         public void SelectOnInvalidResourceReturnsEmptySequence()
         {
-            var actual = validSelector.Select(invalidElementResource).Select(r => r.AsString());
+            var actual = validSelector.Select(invalidResource).Select(r => r.AsString());
             Assert.Empty(actual);
         }
 
         [Fact]
         public void SelectOnStringResourceThrowsInvalidOperationException()
         {
-            Assert.Throws<InvalidOperationException>(() => validSelector.Select(validStringResource));
+            Assert.Throws<InvalidOperationException>(() => validSelector.Select(stringResource));
         }
     }
 }
