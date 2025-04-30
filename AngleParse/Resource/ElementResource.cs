221eaf698bc19255547af380f0818b721eba2d0e
kamome283 <26428614+kamome283@users.noreply.github.com>
2025-04-30 18:50:58 +0900

    #28 mark the ElementResource class as sealed to prevent unintended inheritance.

--- AngleParse/Resource/ElementResource.cs
+++ AngleParse/Resource/ElementResource.cs
@@ -6,7 +6,7 @@ using AngleSharp.Dom;
 
 namespace AngleParse.Resource;
 
-internal class ElementResource(IElement element) : StringResource(element.TextContent)
+internal sealed class ElementResource(IElement element) : StringResource(element.TextContent)
 {
     public IElement Element => element;
 
