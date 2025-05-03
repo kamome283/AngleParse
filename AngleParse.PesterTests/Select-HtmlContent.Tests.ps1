11587411b0695dc835c5ec2dd4dde4b17cd367c1
Tsubasa <26428614+kamome283@users.noreply.github.com>
2025-05-03 15:43:53 +0900

    #18 Add Pester tests for ScriptBlockSelector

--- AngleParse.PesterTests/Select-HtmlContent.Tests.ps1
+++ AngleParse.PesterTests/Select-HtmlContent.Tests.ps1
@@ -23,12 +23,58 @@ BeforeAll {
         if (Test-Path $file)
         {
             $content = Get-Content $file -Raw
-            $cache[$filename] = $content
+            $assetsCache[$filename] = $content
             return $content
         }
         else
         {
             throw "File not found: $file"
+        }
+    }
+}
+
+Describe 'Select-HtmlContent' {
+    Context 'ScriptBlockSelector' {
+        It 'binds $_ in the scriptblock to the current element' {
+            Get-Asset 'full-attribute.html' | Select-HtmlContent {
+                $_ | should -be 'some link'
+            }
+        }
+        It 'outputs the result of the scriptblock' {
+            $result = Get-Asset 'full-attribute.html' | Select-HtmlContent {
+                if ($_ -like 'some link')
+                {
+                    return 1
+                }
+            }
+            $result | should -be 1
+        }
+        It 'outputs multiple results when the scriptblock returns multiple values' {
+            $result = Get-Asset 'full-attribute.html' | Select-HtmlContent {
+                if ($_ -like 'some link')
+                {
+                    return 1, 2
+                }
+            }
+            $result | should -be 1, 2
+        }
+        It 'outputs null when the scriptblock returns nothing' {
+            $result = Get-Asset 'full-attribute.html' | Select-HtmlContent { }
+            $result | should -BeNullOrEmpty
+        }
+        It 'outputs the result even if the Write-Error is called in the scriptblock' {
+            $result = Get-Asset 'full-attribute.html' | Select-HtmlContent {
+                Write-Error -Message "Error in scriptblock" 2> $null
+                return 1
+            }
+            $result | should -be 1
+        }
+        It 'throws if the scriptblock throws an exception' {
+            {
+                Get-Asset 'full-attribute.html' | Select-HtmlContent {
+                    throw "Error in scriptblock"
+                }
+            } | should -throw
         }
     }
 }
