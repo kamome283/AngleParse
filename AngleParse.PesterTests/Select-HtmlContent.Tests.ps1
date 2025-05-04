0458d1c8b24c6d92eda7ba64734c9e88914f2250
Tsubasa <26428614+kamome283@users.noreply.github.com>
2025-05-04 12:14:10 +0900

    #18 Refactored Pester setup to match personal preferences

--- AngleParse.PesterTests/Select-HtmlContent.Tests.ps1
+++ AngleParse.PesterTests/Select-HtmlContent.Tests.ps1
@@ -1,20 +1,18 @@
 BeforeAll {
     $solutionRoot = Split-Path -Parent $PSScriptRoot
-    $implementationRoot = Join-Path $solutionRoot 'AngleParse'
-    $projectFile = Join-Path $implementationRoot 'AngleParse.csproj'
-    dotnet publish $projectFile
+
     if (Get-Module -Name AngleParse)
     {
         Remove-Module AngleParse
     }
-    $moduleFile = Join-Path $implementationRoot 'bin/Release/AngleParse/AngleParse.psd1'
-    Import-Module $moduleFile
+    dotnet publish (Join-Path $solutionRoot 'AngleParse' 'AngleParse.csproj') -c Release
+    Import-Module (Join-Path $solutionRoot 'AngleParse' 'bin/Release/AngleParse/AngleParse.psd1') -Force
 
-    $assetsDir = Join-Path $solutionRoot 'AngleParse.Test/assets'
-    $assetsCache = @{ }
+    $assetsDir = Join-Path $solutionRoot 'AngleParse.Test' 'assets'
+    $cacheTable = @{ }
     function Get-Asset([string]$filename)
     {
-        $cache = $assetsCache[$filename]
+        $cache = $cacheTable[$filename]
         if ($cache)
         {
             return $cache
@@ -22,9 +20,9 @@ BeforeAll {
         $file = Join-Path $assetsDir $filename
         if (Test-Path $file)
         {
-            $content = Get-Content $file -Raw
-            $assetsCache[$filename] = $content
-            return $content
+            $asset = Get-Content $file -Raw
+            $cacheTable[$filename] = $asset
+            return $asset
         }
         else
         {
